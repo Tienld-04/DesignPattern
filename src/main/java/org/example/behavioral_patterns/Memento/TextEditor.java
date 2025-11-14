fa52c95ca8f5fa75658f85144bdd113099adfd1c
LeDanhTien <danhtien2k4@gmail.com>
2025-11-14 10:20:20 +0700

    update memento

--- src/main/java/org/example/behavioral_patterns/Memento/TextEditor.java
+++ src/main/java/org/example/behavioral_patterns/Memento/TextEditor.java
@@ -2,7 +2,7 @@ package org.example.behavioral_patterns.Memento;
 
 // Originator
 class TextEditor {
-    private String content = "";
+    private String content = "";  // state
 
     public void type(String words) {
         content += words + " ";
@@ -10,11 +10,11 @@ class TextEditor {
 
     public String getContent() {
         return content;
-    }
+    } //state hiện tại
 
     public TextMemento save() {
         return new TextMemento(content);
-    }
+    }  // createMemento
 
     public void restore(TextMemento memento) {
         content = memento.getContent();
